c5c5a079f814d515de14810d692c83bcff74a180
Keylin Fargas <keyfargas18@gmail.com>
2023-02-04 06:03:45 -0600

    agregar comentarios al codigo

--- calcular/script.js
+++ calcular/script.js
@@ -3,12 +3,29 @@ const input1 = document.querySelector('#calculo1');
 const input2 = document.querySelector('#calculo2');
 const btn = document.querySelector('#btnCalcular');
 const pResult = document.querySelector('#result');
+const form = document.querySelector('form');
 
 form.addEventListener('submit', sumarInput); //permite escuchar eventos de los usuarios sin mezclar código HTML
+//(A QUIÉN -          QUÉ ACCIÓN - CUAL FUNCION, tipo de evento para escuchar., funcion que debe mandar a llamar)
 
 function sumarInput(event){
-    console.log({event})
-    event.preventDefault();
+    console.log({event});
+    event.preventDefault(); /*es para que el fomulario no haga lo que por defecto hace
+    que es recargar la pagina para enviar en la url lo que escribieron los usuarios, 
+    sino que solamente haga lo que nosotros querramos desde js*/
     var resultado = +input1.value + +input2.value;
     pResult.innerText = "Resultado: "+resultado;
 }
+
+//OTRA MANERA DE HACERLO
+
+/*btn.addEventListener('click', sumarInput);
+
+function sumarInput(event){
+    var resultado = +input1.value + +input2.value;
+    pResult.innerText = "Resultado: "+resultado;
+}*/
+
+/*para que esto funcione tenemos que ir al button y ponerle la propiedad type="button"
+porque por defecto el formulario agarra el ultimo boton para cargar la pagina
+*/
--- script.js
+++ script.js
@@ -17,16 +17,15 @@ console.log({
 h1.innerHTML = 'Patito <br> lala'; //Convierte todo a codigo html
 h1.innerText = 'Hello' //Convierte a texto
 console.log(h1.getAttribute('class')); //accedemos a cualquier atributo
-h1.setAttribute('class', 'amarillo'); //cambamos el nombre de la clase
-
+h1.setAttribute('class', 'amarillo'); //cambiamos el atributo en este caso la clase
 h1.classList.add('verde'); //agrega esto a la clase
 h1.classList.remove('amarillo'); //elimina este elemento de la clase
 
 input.value='234';
 
-const img = document.createElement('img')
+const img = document.createElement('img') //Nos ayuda a crear el elemto html que querramos
 img.setAttribute('src', 'https://evitarquesatanas.com/wp-content/uploads/2019/06/Jesus-name-powerful-2.jpg');
 console.log(img);
 
-pid.innerHTML=""
-pid.append(img);
+pid.innerHTML=""; 
+pid.append(img); //agrega el elemento despues del contenido
